d775a507361e774c4afe8a07762149f083e45696
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2015-12-03 17:54:34 -0500

    Added README

---
+++ Demo0/Demo.js
@@ -0,0 +1,42 @@
+// Constructor for answers
+var Answer = function (data) {
+    this.name = ko.observable(data.name);
+    this.count = ko.observable(0);
+};
+
+var ViewModel = function () {
+
+    // This just makes it easier to understand things.
+    var self = this;
+
+    // Set up our variables
+    self.question = ko.observable("What is your favorite color?");
+    self.answers = ko.observableArray([
+        new Answer({
+            name: "Red"
+        }),
+        new Answer({
+            name: "Blue"
+        }),
+        new Answer({
+            name: "Green"
+        })
+    ]);
+    self.currentView = ko.observable("ask");
+    self.selectedAnswer = ko.observable("");
+    self.totalCount = ko.observable(0);
+
+    // Function to handle a user clicking an answer
+    self.surveyAnswer = function (answer) {
+        self.selectedAnswer(answer.name());
+        var thisAnswer = ko.utils.arrayFirst(self.answers(), function (ans) {
+            return ans.name() === answer.name();
+        });
+        thisAnswer.count(thisAnswer.count() + 1);
+        self.totalCount(self.totalCount() + 1);
+        self.selectedAnswer(answer.name());
+        self.currentView("results");
+    }
+};
+
+ko.applyBindings(new ViewModel());
