9a9aa5a9a9aa2d05596f8bf30c2ac02b18887a28
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2015-12-04 10:41:00 -0500

    Added README

--- demo-1/demo.js
+++ demo-1/demo.js
@@ -1,7 +1,6 @@
 // Constructor for answers
 var Answer = function (data) {
     this.name = ko.observable(data.name);
-    this.count = ko.observable(0);
 };
 
 var ViewModel = function () {
@@ -23,20 +22,11 @@ var ViewModel = function () {
         })
     ]);
     self.selectedAnswer = ko.observable("");
-    self.totalCount = ko.observable(0);
 
     // Function to handle a user clicking an answer
     self.surveyAnswer = function (answer) {
         self.selectedAnswer(answer.name());
-        var thisAnswer = ko.utils.arrayFirst(self.answers(), function (ans) {
-            return ans.name() === answer.name();
-        });
-        thisAnswer.count(thisAnswer.count() + 1);
-        self.selectedAnswer(answer.name());
     }
 };
 
-// Give some time for libraries to load
-setTimeout(function() {
-    ko.applyBindings(new ViewModel());
-}, 500);
+ko.applyBindings(new ViewModel());
